be7791ef235daa06f8c6fd728f3fc0b3c6dad264
shalini <s8115088268@gmail.com>
2021-05-07 14:06:17 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
-
+import Navbar from './components/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
 function App() {
+
   return (
 <>
-<h1>Portfolio</h1>
+
+<Navbar />
 </>
   );
 }
